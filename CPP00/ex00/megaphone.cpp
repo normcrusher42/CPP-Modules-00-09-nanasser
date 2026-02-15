0f071ff7957a345acf609d8b949728741c7c020f
normcrusher42 <nanasser@student.42abudhabi.ae>
2026-02-15 04:55:29 +0400

    cpp00->ex00 smashed. next.

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanasser <nanasser@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/15 03:27:13 by nanasser          #+#    #+#             */
+/*   Updated: 2026/02/15 03:27:13 by nanasser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	int	j;
+	for (int i = 1; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+			std::cout << (char)std::toupper(av[i][j++]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
