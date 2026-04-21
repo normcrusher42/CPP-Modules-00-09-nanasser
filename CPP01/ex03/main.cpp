42579200559e4068a808bf39eb2ae3c0cd95d076
normcrusher42 <nanasser@student.42abudhabi.ae>
2026-04-21 04:51:04 +0400

    ex04 done, one more to go, and its really easy!

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -17,18 +17,18 @@
 int	main()
 {
 	{
-		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);
+		Weapon club = Weapon("Twitter Doomposting");
+		HumanA bob("Trump", club);
 		bob.attack();
-		club.setType("some other type of club");
+		club.setType("Domain Expansion: infinite yapping");
 		bob.attack();
 	}
 	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("Jim");
+		Weapon club = Weapon("Mute button");
+		HumanB jim("John Amongus");
 		jim.setWeapon(club);
 		jim.attack();
-		club.setType("some other type of club");
+		club.setType("\033[0;35mHollow Purple" GRAY);
 		jim.attack();
 	}
 	return (0);
--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -12,10 +12,74 @@
 
 # include <iostream>
 # include <string>
-# include <new>
+# include <fstream>
+
+static void find_and_replace(std::string to_find, std::string to_replace, 
+					std::ifstream &inputFile, std::ofstream &outputFile)
+{
+	std::string 			line;
+	std::string::size_type	found;
+	size_t					end_last_found;
+	std::string				replaced_line;
+
+	end_last_found = 0;
+	while(std::getline(inputFile, line))
+	{
+		replaced_line.clear();
+		end_last_found = 0;
+		found = line.find(to_find);
+		if (found != std::string::npos)
+		{
+			while (found != std::string::npos)
+			{
+				replaced_line.append(line, end_last_found, found - end_last_found);
+				replaced_line += to_replace;
+				end_last_found = found + to_find.length();
+				found = line.find(to_find, end_last_found);
+				if (found == std::string::npos)
+					replaced_line.append(line, end_last_found, line.length());
+			}
+		}
+		else
+			replaced_line = line;
+		if (!inputFile.eof())
+			outputFile << replaced_line << std::endl;
+		else
+			outputFile << replaced_line;
+	}
+}
 
 int	main(int ac, char **av)
 {
+	std::string		inputFileName;
+	std::string		outputFileName;
+	std::ifstream	inputFile;
+	std::ofstream	outputFile;
+	std::string 	to_find;
+	std::string 	to_replace;
+
 	if (ac != 4)
 		return (std::cout << "Argument format: ./gnlpp <filename> <find> <replace>" << std::endl, 1);
+	inputFileName = av[1];
+	to_find = av[2];
+	to_replace = av[3];
+	outputFileName += inputFileName + ".replace";
+
+	inputFile.open(inputFileName.c_str(), std::fstream::in);
+	outputFile.open(outputFileName.c_str(), std::fstream::out);
+	if (!inputFile || !outputFile)
+	{
+		if (!inputFile)
+        	std::cerr << "Wrong input file bruh" << std::endl;
+		if (!outputFile)
+        	std::cerr << "Wrong output file bruh" << std::endl;
+		if (inputFile)
+			inputFile.close();
+		if (outputFile)
+			outputFile.close();
+        return (1);
+	}
+	find_and_replace(to_find, to_replace, inputFile, outputFile);
+	inputFile.close();
+	outputFile.close();
 }
